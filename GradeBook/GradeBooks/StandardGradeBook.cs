76710d1da083921088e93a7a2f756b6fda142d8d
Lisa Caton <lisacaton01@gmail.com>
2020-03-05 14:46:17 -0500

    Step 4 update Type : complete

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,9 +1,11 @@
+
 namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
         {
+            Type = Enums.GradeBookType.Standard;
         }
     }
 }
